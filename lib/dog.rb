dd75ce7bd5845c7bbd38851d2183978bd690c90f
Patrick McCombs <pa.mccombs1@gmail.com>
2018-04-13 00:37:48 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,11 @@
+#Dog
+
+class Dog
+end
+
+fido = Dog.new
+
+snoopy = Dog.new
+
+lassie = Dog.new
+
---
+++ lib/person.rb
@@ -0,0 +1,8 @@
+#Person
+
+class Person
+end
+
+adele_goldberg = Person.new
+
+alan_kay = Person.new
